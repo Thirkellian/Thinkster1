546e5075f56fb3470e92d3daa3206ae973b78f4b
Katye Russell <thirkellian@gmail.com>
2015-10-18 16:08:41 -0500

    Took out redundant angular module declaration - service now functional.

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
+
 .factory('posts', [function(){
   var o = {
     posts: []
   };
   return o;
 }])
-
-angular.module('flapperNews', [])
 
 .controller('MainCtrl', [
   '$scope',
@@ -16,19 +29,19 @@ angular.module('flapperNews', [])
     $scope.posts = posts.posts;
 
   // Allows user to add new posts
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') { return;}
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0
-    });
-    $scope.title = '';
-    $scope.link = '';
-  };
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return;}
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
 
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
-  };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
 
   }]);
