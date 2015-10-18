4dd25368c58e708c64ef06478ea0d672197ba4bc
Katye Russell <thirkellian@gmail.com>
2015-10-18 17:16:04 -0500

    Add comments and commenting

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 angular.module('flapperNews', ['ui.router'])
-
 .config([
 '$stateProvider',
 '$urlRouterProvider',
@@ -10,9 +9,15 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
+
 }])
 
 .factory('posts', [function(){
@@ -22,6 +27,7 @@ function($stateProvider, $urlRouterProvider) {
   return o;
 }])
 
+
 .controller('MainCtrl', [
   '$scope',
   'posts',
@@ -29,13 +35,16 @@ function($stateProvider, $urlRouterProvider) {
   function($scope, posts){
     $scope.posts = posts.posts;
 
-  // Allows user to add new posts
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') { return;}
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -45,4 +54,23 @@ function($stateProvider, $urlRouterProvider) {
       post.upvotes += 1;
     };
 
-  }]);
+}])
+
+.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+
+}]);
